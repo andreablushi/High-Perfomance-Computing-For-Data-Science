454b97d8bf2c63ddf89329e94180ba264a40f6e5
Andrea Blushi <andreablushi@gmail.com>
2025-10-01 22:59:11 +0200

    Updated reduce.c with broadcast and Allreduce

--- homework/reduce.c
+++ homework/reduce.c
@@ -6,6 +6,7 @@
 const int MAX_STRING = 100;
 
 /*
+    REDUCE + REDUCEALL + BROADCAST
     Task:
     Implement a “reduce” by using send and
     receive + sum over the set of data collected
@@ -15,11 +16,13 @@ int main(void) {
     int comm_sz;    /* Number of processes */
     int my_rank;    /* My process rank     */
 
+    int sum = 0; // Sum variable for the master
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    // Basic process
+    // REDUCE PART
+    // Basic process 
     if (my_rank != 0) {
         srand(my_rank);
         int local_value = (rand() % 10) + 1; // Generating a number
@@ -27,7 +30,6 @@ int main(void) {
         MPI_Send(&local_value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     // Master process
     }else{
-        int sum = 0;
         int i;
         for (i = 1; i < comm_sz; i++) {
             int recv_val;
@@ -36,6 +38,20 @@ int main(void) {
         }
         printf("Final sum at master process = %d\n", sum);
     }
+    // BROADCAST PART
+    if (my_rank == 0){
+        int i;
+        for (i = 1; i < comm_sz; i++) {
+            MPI_Send(&sum, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+        printf("The result has been sent to everyone\n");
+
+    }else{
+        // For the other process I receive on the sum variable for convenience
+        MPI_Recv(&sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("I'm process %d and the result is %d!\n", my_rank, sum);
+    }
+    // All the prevoius code summarize the AllReduce program
 
     MPI_Finalize();
     return 0;
